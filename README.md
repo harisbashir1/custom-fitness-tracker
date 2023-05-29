04eefb851d8f1218b031abd205a195aedc0775a3
Haris Bashir <114310104+harisbashir1@users.noreply.github.com>
2023-05-29 16:19:25 -0400

    Initial project setup: Add basic backend, Add .gitignore, Add backend views, Add public directory and basic files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# custom-fitness-tracker
+Custom fitness tracker
+
+Goal:
+
+Learn from creating a full stack web-app, and create a custom, useful fitness tracker.
---
+++ index.js
@@ -0,0 +1,66 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import bcrypt from 'bcrypt'
+import { v4 as uuidv4 } from 'uuid';
+const PORT = 8050;
+const app = express(); 
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine","ejs");
+
+
+app.get('/',(req,res) =>{
+    res.render('login');
+})
+
+app.get('/register',(req,res) =>{
+    res.render('register');
+})
+
+
+app.post('/login', async(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(user => user.username === username);
+
+    if (!user) {
+        return res.status(401).send('Invalid Credentials');
+      }
+    const passwordVerified = await bcrypt.compare(password, user.password);
+    if (passwordVerified) {
+        res.render('dashboard', { name: user.name});
+      } else {
+        res.status(401).send('Invalid Credentials');
+      }
+})
+
+let users = [];
+
+app.post('/register', async(req,res)=>{
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+    const userid = uuidv4();
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = {name: name, username: username, password: hashedPassword, id: userid};
+
+    users.push(newUser);
+    console.log(users);
+    res.render('dashboard',{name: name});
+})
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
